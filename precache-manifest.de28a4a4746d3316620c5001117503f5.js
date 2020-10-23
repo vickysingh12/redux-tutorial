40b8419271334aa466f70d7ee0583ac1cc74cdaa
vickysingh12 <vicky.bhullar1000@gmail.com>
2020-10-23 14:36:01 +0530

    Updates

---
+++ precache-manifest.de28a4a4746d3316620c5001117503f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c740c31183d5cc0445cef8f34fe8f53",
+    "url": "/redux-tutorial/index.html"
+  },
+  {
+    "revision": "16eadff07fdbe8cea4db",
+    "url": "/redux-tutorial/static/css/main.60f3dff1.chunk.css"
+  },
+  {
+    "revision": "031a960376209e171aa5",
+    "url": "/redux-tutorial/static/js/2.87a686e9.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/redux-tutorial/static/js/2.87a686e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16eadff07fdbe8cea4db",
+    "url": "/redux-tutorial/static/js/main.d153e75c.chunk.js"
+  },
+  {
+    "revision": "f0ab3d262dc91be0ecf0",
+    "url": "/redux-tutorial/static/js/runtime-main.24c71a83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
